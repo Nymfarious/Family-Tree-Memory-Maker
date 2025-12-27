88297c30e24a4bec6d49a332b521a02a0b511452
Shannon <49803551+Nymfarious@users.noreply.github.com>
2025-12-26 18:24:45 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Family-Tree-Memory-Maker">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
